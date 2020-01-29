89a94b47f248e54dd1562bc33a4d29244ceb65b1
mouryarahul <mourya.rahul1981@gmail.com>
2020-01-29 12:05:17 +0000

    minor changes

--- micropyboard/Test.py
+++ micropyboard/Test.py
@@ -1,26 +1,9 @@
-try:
-    from machine import I2C
-except ImportError as error:
-    # Output expected ImportErrors.
-    print(error.__class__.__name__ + ": " + str(error))
+from utime import sleep
+from machine import I2C
+from machine import Pin
+from utsys01 import TSYS01
+from ums5837 import MS5837
 
-try:
-    from machine import Pin
-except ImportError as error:
-    # Output expected ImportErrors.
-    print(error.__class__.__name__ + ": " + str(error))
-
-try:
-    from utsys01 import TSYS01
-except ImportError as error:
-    # Output expected ImportErrors.
-    print(error.__class__.__name__ + ": " + str(error))
-
-try:
-    from ums5837 import MS5837
-except ImportError as error:
-    # Output expected ImportErrors.
-    print(error.__class__.__name__ + ": " + str(error))
 
 # Intialize I2C Bus X
 Pin('PULL_SCL', Pin.OUT, value=1)  # enable 5.6kOhm X9/SCL pull-up
@@ -31,18 +14,25 @@ except Exception as error:
     print(error.__class__.__name__ + ": " + str(error))
     i2c = None
 
+
+"""
+# Intialize I2C Bus Y
+y9 = Pin('Y9', Pin.OUT, Pin.PULL_UP)  # enable 5.6kOhm Y9/SCL pull-up
+y10 = Pin('Y10', Pin.OUT, Pin.PULL_UP)  # enable 5.6kOhm Y10/SDA pull-up
+try:
+    i2c = I2C('Y', freq=400000)
+    print(i2c.scan())
+except Exception as error:
+    print(error.__class__.__name__ + ": " + str(error))
+    i2c = None
+"""
+
 # Instantiate TSYS01 (Temperature Sensor)
 try:
     tsys01 = TSYS01(i2c)
 except Exception as error:
     print(error.__class__.__name__ + ": " + str(error))
     tsys01 = None
-
-# Initialize Sensor for reading
-tsys01.init()
-tsys01.read()
-temp = tsys01.temperature()
-print("Temperature from TSYS01: ", temp)
 
 # Instantiate MS5837 (Pressure Sensor)
 try:
@@ -51,10 +41,18 @@ except Exception as error:
     print(error.__class__.__name__ + ": " + str(error))
     ms5837 = None
 
+# Initialize Temperature Sensor for reading
+tsys01.init()
 # Initialize Pressure Sensor for reading
 ms5837.init()
-ms5837.read()
-pressure = ms5837.pressure()
-temperature = ms5837.temperature()
-print("Temperature from MS5837: ", temperature)
-print("Pressure from MS5837: ", pressure)
+for i in range(10):
+    tsys01.read()
+    ms5837.read()
+    temperature_1 = tsys01.temperature()
+    pressure = ms5837.pressure()
+    temperature_2 = ms5837.temperature()
+    print("Temperature in Centrigrade from TSYS01: {:0.4f} and from MS5837: {:0.4f}".format(temperature_1, temperature_2))
+    print("Pressure in mBar from MS5837: {:0.4f}".format(pressure))
+    sleep(1.0)
+
+
